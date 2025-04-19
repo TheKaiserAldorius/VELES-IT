f6e8020b1e36c1d705e285912381c389d14796dc
German Fridrich <h.frydrykh@sava.team>
2025-04-19 13:57:48 +0300

    поправлен адаптив для калькулятора и старицы его

--- frontend/src/pages/calculator/CalculatorPage.js
+++ frontend/src/pages/calculator/CalculatorPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import CalculatorForm from './CalculatorForm';
-import './CalculatorPage.css';
+import React from "react";
+import CalculatorForm from "./CalculatorForm";
+import "./CalculatorPage.css";
 
 const CalculatorPage = () => {
   return (
